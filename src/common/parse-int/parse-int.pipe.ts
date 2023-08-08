fd17d93e9e3a4d05d35b3e4742910a653de61294
Fabianormo <fabianormo@outlook.com>
2023-08-08 11:42:58 -0500

    feat: 🎸 Clase 20: validando con class-validator y mapped-types

--- src/common/parse-int/parse-int.pipe.ts
+++ src/common/parse-int/parse-int.pipe.ts
@@ -7,6 +7,7 @@ import {
 
 @Injectable()
 export class ParseIntPipe implements PipeTransform {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   transform(value: string, metadata: ArgumentMetadata) {
     // valor en base 10
     const val = parseInt(value, 10);
